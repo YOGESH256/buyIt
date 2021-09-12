f0d03bb76ba17317fc9b7d40e3e495bc733cb5a7
YOGESH256 <2019yogesh.khatri@ves.ac.in>
2021-09-12 19:57:20 +0530

    create a sample product and update it

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,4 +31,83 @@ else {
 }
 
 })
-export {getProducts , getProductById}
+
+
+
+
+//@desc delete a product
+//@route DELETE /api/products/:id
+//@access  Private/Admin
+const deleteProduct  = asyncHandler(async(req, res) => {
+  const product = await Product.findById(req.params.id);
+  console.log(product);
+
+  if(product)
+  {
+    await product.remove()
+  res.json({message : 'Product Removed'})
+}
+else {
+  res.status(404);
+  throw new Error('Product not found')
+}
+
+})
+
+
+
+
+//@desc Create a product
+//@route POST /api/products/
+//@access  Private/Admin
+const createProduct  = asyncHandler(async(req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price :0,
+    user:req.user._id,
+    image :'/images/sample.jpg',
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock :0,
+    numReviews:0,
+    description:'Sample Description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201)
+  res.json(createdProduct)
+})
+
+
+
+
+//@desc Update a product
+//@route PUT /api/products/:id
+//@access  Private/Admin
+const updateProduct  = asyncHandler(async(req, res) => {
+const {name , price ,description , image ,brand , category, countInStock } = req.body
+
+const product = await Product.findById(req.params.id)
+
+if(product)
+{
+  product.name = name,
+  product.description = description,
+  product.price = price,
+  product.category = category,
+  product.image = image,
+  product.brand = brand,
+  product.countInStock = countInStock
+
+}
+else {
+  res.status(404)
+  throw new Error('Product not found')
+}
+
+  const updatedProduct = await product.save()
+  res.status(201)
+  res.json(updatedProduct);
+})
+
+export {getProducts , getProductById , deleteProduct , updateProduct , createProduct}
