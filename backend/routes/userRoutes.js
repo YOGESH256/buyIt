6cae8876c698b77f154030a7a8f967b0d7af2b22
YOGESH256 <2019yogesh.khatri@ves.ac.in>
2021-09-12 20:02:35 +0530

    update user and delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express'
 const router = express.Router()
-import {protect} from '../middleware/authMiddleware.js'
-import {authUser , getUserProfile, registerUser , updateUserProfile} from '../controllers/userController.js'
+import {protect , admin} from '../middleware/authMiddleware.js'
+import {authUser
+  , getUserProfile,
+   registerUser ,
+    updateUserProfile ,
+     getUsers,
+      deleteUser,
+    getUserById ,
+  updateUser} from '../controllers/userController.js'
 
+router.route('/').get(protect ,admin ,getUsers)
 router.post('/login' , authUser);
 router.route('/profile').get(protect ,getUserProfile).put(protect , updateUserProfile)
 router.route('/').post(registerUser)
+router.route('/:id').delete(protect , admin , deleteUser).get(protect , admin , getUserById).put(protect , admin , updateUser)
 
 
 
